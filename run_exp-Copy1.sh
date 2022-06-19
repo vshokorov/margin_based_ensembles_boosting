5573450c022dc010436954ad65882addf8296d86
vshokorov <slava.schokorov@yandex.ru>
2022-06-19 10:18:07 +0300

    save run_exp*

--- run_exp-Copy1.sh
+++ run_exp-Copy1.sh
@@ -4,7 +4,7 @@ for gap in 0 1 2 3 4 5 6 7 8 9 10 12 14 16 18 20 25 30 35 40 45 50
 do
     python3 train.py --dir=logs/oct \
                      --data_path=./data/ \
-                     --dataset=CIFAR100 \
+                     --dataset=CIFAR10 \
                      --transform=ResNet_9_noDA \
                      --batch_size=500 \
                      --model=ResNet18 \
@@ -16,8 +16,8 @@ do
                      --optimizer=Adam \
                      --lr-shed=OneCycleLR \
                      --dropout 0.5 \
-                     --comment "ResNet18_gap_${gap}_CIFAR100_noDA" \
-                     --wandb_group "ResNet18_margin_in_logits_CIFAR100" \
+                     --comment "ResNet18_gap_${gap}_CIFAR10_noDA" \
+                     --wandb_group "ResNet18_margin_in_logits_CIFAR10" \
                      --gap_size ${gap} \
                      --seed 25477 \
                      --width 16 \
@@ -30,7 +30,7 @@ for gap in 0 1 2 3 4 5 6 7 8 9 10 12 14 16 18 20 25 30 35 40 45 50
 do
     python3 train.py --dir=logs/oct \
                      --data_path=./data/ \
-                     --dataset=CIFAR100 \
+                     --dataset=CIFAR10 \
                      --transform=ResNet_9 \
                      --batch_size=500 \
                      --model=ResNet18 \
@@ -42,8 +42,8 @@ do
                      --optimizer=Adam \
                      --lr-shed=OneCycleLR \
                      --dropout 0.5 \
-                     --comment "ResNet18_gap_${gap}_CIFAR100" \
-                     --wandb_group "ResNet18_margin_in_logits_CIFAR100" \
+                     --comment "ResNet18_gap_${gap}_CIFAR10" \
+                     --wandb_group "ResNet18_margin_in_logits_CIFAR10" \
                      --gap_size ${gap} \
                      --seed 25477 \
                      --width 16 \
--- run_exp-Copy3.sh
+++ run_exp-Copy3.sh
@@ -1,37 +1,10 @@
 # run: CUDA_VISIBLE_DEVICES=<номер GPU> <команда>
 
-
-for gap in 0 0.5 1 1.5 2 2.5 3 3.5 4 4.5 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+for gap in 0 1 2 3 4 5 6 7 8 9 10 12 14 16 18 20 25 30 35 40 45 50
 do
     python3 train.py --dir=logs/oct \
                      --data_path=./data/ \
-                     --dataset=CIFAR100 \
-                     --transform=ResNet_9_noDA \
-                     --batch_size=500 \
-                     --model=ResNet9 \
-                     --save_freq=51 \
-                     --print_freq=5 \
-                     --epochs=50 \
-                     --wd=0.001 \
-                     --lr=0.0032 \
-                     --optimizer=Adam \
-                     --lr-shed=OneCycleLR \
-                     --dropout 0.5 \
-                     --comment "ResNet9_gap_${gap}_CIFAR100_noDA" \
-                     --wandb_group "ResNet9_margin_in_logits_CIFAR100" \
-                     --gap_size $gap \
-                     --seed 25477 \
-                     --width 64 \
-                     --num-nets=2 \
-                     --num-exps=1
-done
-
-
-for gap in 0 0.5 1 1.5 2 2.5 3 3.5 4 4.5 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
-do
-    python3 train.py --dir=logs/oct \
-                     --data_path=./data/ \
-                     --dataset=CIFAR100 \
+                     --dataset=SVHN \
                      --transform=ResNet_9_noDA \
                      --batch_size=500 \
                      --model=ResNet18 \
@@ -39,39 +12,39 @@ do
                      --print_freq=5 \
                      --epochs=50 \
                      --wd=0.001 \
-                     --lr=0.01 \
+                     --lr=0.004 \
                      --optimizer=Adam \
                      --lr-shed=OneCycleLR \
                      --dropout 0.5 \
-                     --comment "ResNet18_gap_${gap}_CIFAR100_noDA" \
-                     --wandb_group "ResNet18_margin_in_logits_CIFAR100" \
-                     --gap_size $gap \
+                     --comment "ResNet18_gap_${gap}_SVHN_noDA" \
+                     --wandb_group "ResNet18_margin_in_logits_SVHN" \
+                     --gap_size ${gap} \
                      --seed 25477 \
-                     --width 64 \
+                     --width 16 \
                      --num-nets=2 \
                      --num-exps=1
 done
 
 
-for gap in 0 0.5 1 1.5 2 2.5 3 3.5 4 4.5 5 6 7 8 9 10
+for gap in 0 1 2 3 4 5 6 7 8 9 10 12 14 16 18 20 25 30 35 40 45 50
 do
     python3 train.py --dir=logs/oct \
                      --data_path=./data/ \
-                     --dataset=CIFAR10 \
-                     --transform=VGG_noDA \
+                     --dataset=SVHN \
+                     --transform=ResNet_9 \
                      --batch_size=500 \
-                     --model=VGG16 \
+                     --model=ResNet18 \
                      --save_freq=51 \
                      --print_freq=5 \
                      --epochs=50 \
                      --wd=0.001 \
-                     --lr=0.0032 \
+                     --lr=0.004 \
                      --optimizer=Adam \
                      --lr-shed=OneCycleLR \
                      --dropout 0.5 \
-                     --comment "width16_gap_${gap}_CIFAR10_noDA" \
-                     --wandb_group "width16_margin_in_logits_CIFAR10" \
-                     --gap_size $gap \
+                     --comment "ResNet18_gap_${gap}_SVHN" \
+                     --wandb_group "ResNet18_margin_in_logits_SVHN" \
+                     --gap_size ${gap} \
                      --seed 25477 \
                      --width 16 \
                      --num-nets=2 \
